a17aef441a59816bfe2e5006ba8327d956fc66aa
sunnykasalawat <sunnykasalawat007@gmail.com>
2019-05-14 11:59:04 +0545

    second commit

---
+++ app/src/main/java/com/e/vechicle_break_downassistance/Fragments/Register_1.java
@@ -0,0 +1,12 @@
+package com.e.vechicle_break_downassistance.Fragments;
+
+import android.support.v4.app.Fragment;
+
+
+public class Register_1 extends Fragment {
+
+    public Register_1() {
+        // Required empty public constructor
+    }
+
+}
---
+++ app/src/main/java/com/e/vechicle_break_downassistance/Fragments/Register_3.java
@@ -0,0 +1,11 @@
+package com.e.vechicle_break_downassistance.Fragments;
+import android.support.v4.app.Fragment;
+
+
+public class Register_3 extends Fragment {
+
+    public Register_3() {
+        // Required empty public constructor
+    }
+
+}
